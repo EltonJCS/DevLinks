e164aec1f07e8bb07f645401aeaf76fdccdfe7a3
EltonJCS <80626909+EltonJCS@users.noreply.github.com>
2023-09-05 15:08:27 -0300

    Modified theme image change from css to javascript

--- js/toggleTheme.js
+++ js/toggleTheme.js
@@ -1,3 +1,12 @@
 function toggleTheme() {
-  document.body.classList.toggle("light-theme");
+  const body = document.body;
+  const profileImage = document.querySelector("#profile img");
+
+  body.classList.toggle("light-theme");
+
+  if (body.classList.contains("light-theme")) {
+    profileImage.setAttribute("src", "../assets/avatar-light.png");
+  } else {
+    profileImage.setAttribute("src", "../assets/avatar.png");
+  }
 }
